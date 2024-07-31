c53efeb1ca7856b960d663531533deb35a65cc55
Michelle Bell <michbesch0@outlook.com>
2024-07-31 15:07:43 -0400

    Add Reaction Schema

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+
+function formatDate(date) {
+    return date.toLocaleString(); // You can customize this format as needed
+}
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        }
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = assignmentSchema;
