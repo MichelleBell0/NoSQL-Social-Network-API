3f6842124f982c7cf157710c56a26f810d9658d8
Michelle Bell <michbesch0@outlook.com>
2024-07-31 14:55:09 -0400

    Add Thought model and update User model

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+function formatDate(date) {
+    return date.toLocaleString(); // You can customize this format as needed
+}
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
